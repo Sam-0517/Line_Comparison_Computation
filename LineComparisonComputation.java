daee672fee84537bc704b76aea91740e8ca25e94
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-23 21:51:48 +0530

    Added : Welcome Message

---
+++ LineComparisonComputation.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.LineComparison;
+
+public class LineComparisonComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
