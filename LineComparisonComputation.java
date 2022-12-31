a612767b12c3c4ff16e4478d17ca9fe372daa493
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-31 08:24:49 +0530

    Added: code for compare lones using Equals and compareTo method

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,49 +1,52 @@
 package com.BridgeLabz.LineComparison;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LineComparisonComputation {
 	Scanner scan = new Scanner(System.in);
-	float x1, y1, x2, y2;
-	static float length;
+	int x1, y1, x2, y2;
+	static int length;
 
-	public float lineCoordinates() {
+	public int lineCoordinates() {
 		System.out.println("Enter point for x1 and y1");
-		x1 = scan.nextFloat();
-		y1 = scan.nextFloat();
+		x1 = scan.nextInt();
+		y1 = scan.nextInt();
 
 		System.out.println("Enter points for x2 and y2 ");
-		x2 = scan.nextFloat();
-		y2 = scan.nextFloat();
+		x2 = scan.nextInt();
+		y2 = scan.nextInt();
 
-		length = (float) Math.sqrt((Math.pow((x2 - x1), 2))
-				+ (Math.pow((y2 - y1), 2)));
+		length = (int) Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
 		return length;
 
 	}
 
 	public static void main(String[] args) {
+
 		LineComparisonComputation obj = new LineComparisonComputation();
 
-		float length1, length2;
-		
+		int length1, length2;
+
 		length1 = obj.lineCoordinates();
+		Integer line1 = length1;
 		// obj.lineCoordinates();
 		System.out.println("Enter coordinates for second line ");
-		
+
 		length2 = obj.lineCoordinates();
+		Integer line2 = length2;
 		// obj.lineCoordinates();
 		System.out.println("Length of line one is " + length1);
 		System.out.println("Length of line two is " + length2);
+
+		int result = line1.compareTo(line2);
 		
-		if(length1 > length2){
+		if (result > 0) {
 			System.out.println("Line1 is greater than line2 ");
-		}
-		else if(length1 < length2 )
+		} else if (result < 0)
 			System.out.println("Line1 is lesser than line2 ");
-		
+
 		else
 			System.out.println("Both lines are equal ");
-	}
 
+	}
 }
